c9ef34d49f1cb63875d13b539848837039f38e58
mykeld74 <mike@msdwebsolutions.com>
2025-02-11 06:52:32 -0700

    Set up basic structure and add header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,52 @@
+<script>
+	import Logo from '$lib/components/Logo.svelte';
+
+	const pages = [
+		{ name: 'Services', href: '/services' },
+		{ name: 'About', href: '/about' },
+		{ name: 'Contact', href: '/contact' },
+		{ name: 'FAQ', href: '/faq' }
+	];
+</script>
+
+<header>
+	<div class="logoContainer">
+		<a href="/">
+			<Logo />
+		</a>
+	</div>
+	<nav>
+		<ul>
+			{#each pages as page}
+				<li><a href={page.href}>{page.name}</a></li>
+			{/each}
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem 2rem;
+	}
+
+	.logoContainer {
+		width: 5rem;
+		--taglineDisplay: none;
+		--companyNameDisplay: none;
+		aspect-ratio: 1 / 1;
+		overflow: hidden;
+	}
+
+	nav ul {
+		display: flex;
+		gap: 2rem;
+	}
+
+	nav ul li {
+		list-style: none;
+		font-size: 1.2rem;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { fade } from 'svelte/transition';
+	import Header from '$lib/components/Header.svelte';
+	import '$lib/css/reset.css';
+	import '$lib/css/styles.css';
+	let { data, children } = $props();
+</script>
+
+<Header />
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+{#key data.pathname}
+	<div in:fade={{ duration: 150, delay: 155 }} out:fade={{ duration: 150 }} class="contentWrapper">
+		{@render children?.()}
+	</div>
+{/key}
