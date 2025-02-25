55999590a3e6ed09792ab44cba1c992ad95b138a
mykeld74 <mike@msdwebsolutions.com>
2025-02-25 06:44:09 -0700

    Add about me content

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -13,48 +13,38 @@
 
 <div class="contentContainer">
 	<p>
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quisquam assumenda quia tempora
-		harum in quasi ullam debitis, temporibus quas numquam expedita sunt et, recusandae culpa
-		eligendi quod fugiat doloribus. Modi ducimus aliquid esse eveniet et ea eius sunt, vitae
-		temporibus. Cumque possimus aliquam perspiciatis fugiat necessitatibus, qui porro neque aliquid
-		voluptatum magni, dolores, vero labore quo eaque ullam minima! Omnis sequi, assumenda deleniti
-		labore odio id eveniet officiis earum laboriosam accusantium dolor explicabo minus quidem
-		nostrum consequuntur ab similique quae non officia nesciunt sapiente distinctio maxime
-		voluptate. Ipsum, atque!
-	</p>
-
-	<p>
-		Nobis, numquam delectus amet quia quasi debitis labore omnis, quibusdam est recusandae
-		necessitatibus modi quisquam magni maxime explicabo voluptatum non? Ullam ex officiis quasi,
-		eius illum adipisci laborum doloribus? Tempore! Vitae itaque eaque perspiciatis praesentium
-		tempora molestiae maxime officiis consequuntur a illum, odio deleniti, quas autem, vel
-		perferendis recusandae corporis fugit officia quibusdam excepturi? Beatae repellendus pariatur
-		quisquam voluptatem inventore. Tenetur officia qui quibusdam, repellendus necessitatibus
-		voluptas ab magnam minima velit, nulla dolores eum iure in temporibus dicta consequuntur omnis.
-		Ratione quidem fugit reiciendis harum excepturi a aspernatur provident consequuntur.
+		I have always had a deep passion for healthcare and a commitment to making medical services more
+		accessible and convenient for everyone. Throughout my career, I have seen firsthand how
+		difficult it can be for patients—especially those who are sick, elderly, or have limited
+		mobility—to access essential lab services. Too often, patients struggle to reach a draw station,
+		sometimes waiting hours for transportation just to get the lab work their doctors have ordered.
+		Seeing these challenges inspired me to take action.
 	</p>
 	<div class="imgContainer right"><img src={BloodDrawB} alt="Blood Draw" /></div>
 	<p>
-		Impedit voluptates perspiciatis ratione a corrupti odit placeat facilis consequuntur, cumque
-		tempora voluptatibus ad iste illo, aut odio provident sint, unde quisquam itaque! Ratione id in
-		ipsa laborum fugiat accusamus. Provident harum sapiente eligendi dignissimos incidunt iste
-		itaque unde ipsum expedita iusto magni quaerat, id tempora molestias obcaecati quo delectus
-		aperiam consequatur est. Facere doloribus ex officiis suscipit asperiores. Quisquam!
+		That’s why I founded Helping Hands Mobile Phlebotomy—to bring the lab to you. My goal is to
+		provide a compassionate, stress-free experience by offering high-quality phlebotomy services in
+		the comfort of your home. With experience spanning both pediatric and geriatric care, I’ve had
+		the privilege of working with patients at every stage of life. My journey began in pediatrics,
+		where I spent years supporting children and their families in hospitals and outpatient clinics.
+		Later, I transitioned to geriatric care, working in nursing homes and assisted living facilities
+		to enhance the quality of life for older adults through compassionate, patient-centered care.
 	</p>
 	<p>
-		Quae, sed molestias doloribus iste odio tenetur eligendi temporibus voluptates rem quaerat et
-		dolore obcaecati explicabo voluptatibus sapiente reiciendis ducimus. Sequi facilis mollitia
-		culpa eligendi eaque dolores doloremque laborum porro.
-	</p>
-	<p>
-		In voluptate dolor iure quos quas. Porro totam assumenda quo veritatis impedit nam, eius
-		distinctio vel amet dolorem aspernatur, ducimus molestiae sint pariatur repellendus ea labore,
-		debitis dignissimos neque enim.
+		I believe that no one should have to choose between their health and the challenges of getting
+		to a lab. At Helping Hands Mobile Phlebotomy, I am dedicated to ensuring that you receive the
+		care you need—wherever you are.
 	</p>
 </div>
 
 <style>
 	.imgContainer {
-		width: 50%;
+		width: 30%;
+		@media (max-width: 500px) {
+			width: 100%;
+			.right {
+				float: none;
+			}
+		}
 	}
 </style>
